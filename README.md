428bfe5f5c19283bf3d3e30b6f2f454d2c1b6879
Vince Kurtz <vjkurtz@gmail.com>
2020-11-14 14:54:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kinvoa Drake
+
+Code for controlling a Kinova Gen3 Manipulator via Drake.
---
+++ simulate.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from pydrake.all import *
+
+# Find the (local) description file relative to drake
+robot_description_path = "./model/urdf/GEN3_URDF_V12.urdf"
+drake_path = getDrakePath()
+robot_description_file = "drake/" + os.path.relpath(robot_description_path, start=drake_path)
+
+# Load the kuka model from a urdf file
+robot_urdf = FindResourceOrThrow(robot_description_file)
+builder = DiagramBuilder()
+scene_graph = builder.AddSystem(SceneGraph())
+dt = 1e-3
+plant = builder.AddSystem(MultibodyPlant(time_step=dt))
+plant.RegisterAsSourceForSceneGraph(scene_graph)
+kgen3 = Parser(plant=plant).AddModelFromFile(robot_urdf,"kinova_gen3")
+
+# Fix the base of the manipulator to the world
+plant.WeldFrames(plant.world_frame(),plant.GetFrameByName("base_link",kgen3))
+
+# Add a flat ground with friction
+X_BG = RigidTransform()
+surface_friction = CoulombFriction(
+        static_friction = 0.7,
+        dynamic_friction = 0.1)
+plant.RegisterCollisionGeometry(
+        plant.world_body(),      # the body for which this object is registered
+        X_BG,                    # The fixed pose of the geometry frame G in the body frame B
+        HalfSpace(),             # Defines the geometry of the object
+        "ground_collision",      # A name
+        surface_friction)        # Coulomb friction coefficients
+plant.RegisterVisualGeometry(
+        plant.world_body(),
+        X_BG,
+        HalfSpace(),
+        "ground_visual",
+        np.array([0.5,0.5,0.5,0.0]))    # Color set to be completely transparent
+
+#plant.set_contact_model(ContactModel.kHydroelasticWithFallback)
+
+plant.Finalize()
+assert plant.geometry_source_is_registered()
+
+# Set up the Scene Graph
+builder.Connect(
+        scene_graph.get_query_output_port(),
+        plant.get_geometry_query_input_port())
+builder.Connect(
+        plant.get_geometry_poses_output_port(),
+        scene_graph.get_source_pose_port(plant.get_source_id()))
+
+# Connect controllers
+controller = builder.AddSystem( ConstantVectorSource(np.zeros(plant.num_actuators())) )
+builder.Connect(                              # First controller output is torques to the arm
+        controller.get_output_port(0),
+        plant.get_actuation_input_port(kgen3))
+
+# Set up the Visualizer
+DrakeVisualizer().AddToBuilder(builder=builder, scene_graph=scene_graph)
+ConnectContactResultsToDrakeVisualizer(builder, plant)
+
+# Compile the diagram: no adding control blocks from here on out
+diagram = builder.Build()
+diagram.set_name("diagram")
+diagram_context = diagram.CreateDefaultContext()
+
+# Visualize the diagram
+#plt.figure()
+#plot_system_graphviz(diagram,max_depth=2)
+#plt.show()
+
+# Simulator setup
+simulator = Simulator(diagram, diagram_context)
+simulator.set_target_realtime_rate(0.1)
+simulator.set_publish_every_time_step(False)
+
+# Set initial states
+plant_context = diagram.GetMutableSubsystemContext(plant, diagram_context)
+plant.SetPositions(plant_context, kgen3, np.array([0.1,0,0,np.pi/2,-0.1,0,0]))   # Manipulator arm
+plant.SetVelocities(plant_context, kgen3, np.zeros(7))
+
+# Run the simulation
+simulator.Initialize()
+simulator.AdvanceTo(3.00)
+
