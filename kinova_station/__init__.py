08dacc9c93ce44c84897ee66177d44e48562328e
Vince Kurtz <vjkurtz@gmail.com>
2021-06-20 22:02:22 -0400

    ignore hardware interface if kortex API not installed

--- kinova_station/__init__.py
+++ kinova_station/__init__.py
@@ -1,3 +1,8 @@
 from kinova_station.simulation_station import KinovaStation
-from kinova_station.hardware_station import KinovaStationHardwareInterface
 from kinova_station.common import EndEffectorTarget, GripperTarget
+try:
+    from kinova_station.hardware_station import KinovaStationHardwareInterface
+except ImportError:
+    # Don't import the hardware interface if the kortex API isn't installed
+    print("Kortex API not decected: disabling hardware interface")
+    pass
